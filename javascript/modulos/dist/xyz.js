1efdf22fe0eeedcd06d602a4deeeb9f5ee5e1ee0
begotrujano <diegotrujano3c@gmail.com>
2026-05-18 12:19:57 -0600

    distribucion

---
+++ javascript/modulos/dist/xyz.js
@@ -0,0 +1,2 @@
+(()=>{function n(o){return console.log("y:"+o),"fin y"}function r(o){return console.log("z:"+o),"fin z"}console.log(miModulo.x("abc"));console.log(miModulo.xx("def"));console.log(n("ghi"));console.log(r("jkl"));})();
+//# sourceMappingURL=xyz.js.map

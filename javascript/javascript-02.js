b63a32738a6c6fda21bf5fa49e820bc24300b8f5
begotrujano <diegotrujano3c@gmail.com>
2026-05-18 12:16:01 -0600

    js con y sin modulos

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -1,7 +1,8 @@
 let resultado = document.getElementById("resultado")
 let botones = document.getElementsByTagName("button")
-let prm1 = 0
+let prm1 = null
 let operacion = ""
+let nuevoNumero = false
 
 for (const key in botones) {
     if (Object.prototype.hasOwnProperty.call(botones, key)) {
@@ -14,29 +15,43 @@ for (const key in botones) {
 }
 
 function pintar(e){
+    if (nuevoNumero) {
+        resultado.value = ""
+        nuevoNumero = false
+    }
     resultado.value += e.target.innerText
 }
 
 function pintar2(e){
     let signo = e.target.innerText
+    
     if (signo == "C") {
         resultado.value = ""
-        prm1 = 0
+        prm1 = null
         operacion = ""
-    } else if (signo == "=") {
-        igual()
-    } else {
+        return
+    }
+
+    if (prm1 === null) {
         prm1 = resultado.value
+    } else if (operacion) {
+        igual()
+        prm1 = resultado.value
+    }
+
+    if (signo == "=") {
+        prm1 = null
+        operacion = ""
+    } else {
         operacion = signo
-        resultado.value = ""
+        nuevoNumero = true
     }
 }
 
 function igual(){
-    let prm2 = resultado.value
-    let final = 0
     let n1 = parseFloat(prm1)
-    let n2 = parseFloat(prm2)
+    let n2 = parseFloat(resultado.value)
+    let final = 0
 
     if (operacion == "+") final = n1 + n2
     if (operacion == "-") final = n1 - n2
